93bd0f18bd84167ae16aa152a9cb332badcb1bcc
Rodrigo <rodrigudesousa@gmail.com>
2025-08-29 18:05:57 -0400

    app created

---
+++ src/main/java/com/rodrigosousa/oraclecloud_microservice/OraclecloudMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigosousa.oraclecloud_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraclecloudMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraclecloudMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigosousa/oraclecloud_microservice/OraclecloudMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigosousa.oraclecloud_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraclecloudMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
